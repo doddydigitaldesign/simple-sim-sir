97c92206c060750a95be78a1142c86dcd0998222
Daniel Sonne Lehnberg <doddydigitaldesign@gmail.com>
2020-03-29 20:06:27 +0200

    Updates

---
+++ precache-manifest.c1a255d91970ce9b6137a44b48b79f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410b13be2b84f45b744902a4d7d0199e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b92f21765376190ade2",
+    "url": "/static/css/main.7983955d.chunk.css"
+  },
+  {
+    "revision": "8626222e0587ae4537f8",
+    "url": "/static/js/2.1d489beb.chunk.js"
+  },
+  {
+    "revision": "4a6eab9bf31c22112c37c96cf1088b86",
+    "url": "/static/js/2.1d489beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b92f21765376190ade2",
+    "url": "/static/js/main.5188ae4f.chunk.js"
+  },
+  {
+    "revision": "a67c55e082d23755d9f0",
+    "url": "/static/js/runtime-main.12bc67b3.js"
+  }
+]);
