427a7ce1eb2c994666c04e80aaef16e08a9a0c43
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-04-02 07:49:48 +0200

    Updates

---
+++ precache-manifest.a998df975f6886ef8b7a3cf6feba93d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629cb2400c647a3ab8c4737e64e72fbb",
+    "url": "/simple-sim-sir/index.html"
+  },
+  {
+    "revision": "4d6b1903edb9ada99c1e",
+    "url": "/simple-sim-sir/static/css/main.54a2121e.chunk.css"
+  },
+  {
+    "revision": "ef9aefec24645b66ce87",
+    "url": "/simple-sim-sir/static/js/2.b9015f3e.chunk.js"
+  },
+  {
+    "revision": "4a6eab9bf31c22112c37c96cf1088b86",
+    "url": "/simple-sim-sir/static/js/2.b9015f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6b1903edb9ada99c1e",
+    "url": "/simple-sim-sir/static/js/main.a83904f8.chunk.js"
+  },
+  {
+    "revision": "f622b5c905c7c2960354",
+    "url": "/simple-sim-sir/static/js/runtime-main.40985747.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-sim-sir/precache-manifest.34f2fc0f545786d10829455912251dc5.js"
+  "/simple-sim-sir/precache-manifest.a998df975f6886ef8b7a3cf6feba93d8.js"
 );
 
 self.addEventListener('message', (event) => {
